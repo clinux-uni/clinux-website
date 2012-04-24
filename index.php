7a22bf42a4e9d9a2e0c3d31ed1658d7c7ccb22b6
Hosmel Quintana <hosmelq@gmail.com>
2012-04-24 15:32:01 -0600

    Agregar archivos necesarios

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="es"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="es"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="es"> <!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title></title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width">
+<link rel="stylesheet" href="css/style.css">
+<script src="js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+
+<body>
+<div id="container">
+<header>
+<?php require_once 'includes/header.php'; ?>
+</header>
+<section id="content">
+</section>
+<footer>
+<?php require_once 'includes/footer.php'; ?>
+</footer>
+</div>
+<script src="js/loads.js"></script>
+</body>
+</html>
