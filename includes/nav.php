d5caea74afad50f47564b0b2567a57a81d084abb
River Martinez <rianmartinez@gmail.com>
2012-04-27 15:26:47 -0600

    pequenos cambios en la navegacion y boton beta

--- includes/nav.php
+++ includes/nav.php
@@ -1,8 +1,13 @@
 <h3>Categorias</h3>
 <ul>
-    <?php for ($i = 1; $i <= 8; $i++) { ?>
-    <li><a href="#" title="#">Link <?php echo $i; ?></a></li>
-    <?php } ?>
+    <li><a href="#" title="#">Inicio</a></li>
+    <li><a href="#" title="#">Entradas del Blog</a></li>
+    <li><a href="#" title="#">Lo m&aacute;s visto</a></li>
+    <li><a href="#" title="#">Videotutoriales</a></li>
+    <li><a href="#" title="#">Manuales</a></li>
+    <li><a href="#" title="#"></a></li>
+    <li><a href="#" title="#">Quienes Somos</a></li>
+    <li><a href="#" title="#">Contactanos</a></li>
 </ul>
 <form action="" method="">
     <input type="text" name="search">
