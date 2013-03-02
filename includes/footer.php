179f24a45a6f677dbef868b8859a080dafa6e4d3
Jorge Vallecillo <jorgevallecilloc@gmail.com>
2013-03-02 13:55:23 -0600

    Agregando referencia a este repositorio en el footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,2 +1,3 @@
 <a href="#" title="#"><img src="img/cc.jpg" alt="Creative Commons"></a>
 <p><abbr>clinux</abbr> Page by Comunidad Linux <abbr>uni</abbr> is  licensed under a Creative Commons Reconocimiento-NoComercial-CompartirIgual 3.0 Unported Licensed</p>
+<p>C&oacute;digo fuente del sitio disponible en <a href="https://github.com/clinux-uni/clinux"> github</a></p>
