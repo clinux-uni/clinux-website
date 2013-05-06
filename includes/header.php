baee95da439711da5d3c65e5d740abbbf8bc0e38
Samuel J Gutierrez Aviles <search.sama@gmail.com>
2013-05-06 04:37:43 -0600

    lipiesa de los directorios de la rama para que solo existan los directorios de la framework

--- includes/header.php
+++
@@ -1,5 +0,0 @@
-<hgroup class="container">
-    <h1><a href="./" title="Inicio">Clinux</a></h1>
-    <h2>Comunidad linux <abbr title="Universidad Nacional de Ingenier&iacute;a">uni</abbr></h2>
-    <p><img class="beta" src="img/beta.png" title="Esta p&aacute;gina esta en su versi&oacute;n Beta"></p>
-</hgroup>
