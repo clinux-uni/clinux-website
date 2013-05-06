b606d9ee6a52f619e5dc3041483a556b0f58d739
Samuel J Gutierrez Aviles <search.sama@gmail.com>
2013-05-06 02:29:26 -0600

    subida de framework CakePHP en blando

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
-clinux
-======
+CakePHP
+=======
 
-Código fuente del sitio de la Comunidad de usuarios de linux de la Universidad Nacional de Ingeniería (Nicaragua)
-Sitio en: http://clinux.uni.edu.ni
+Esto es lo que necesitas:
+
+Servidor web funcionando. Asumiremos que estás usando Apache, aunque las instrucciones para otros servidores son similares. Igual tendremos que ajustar un poco la configuración inicial, pero todos los pasos son sencillos. La mayor parte de nosotros podrá tener CakePHP funcionando sin tocar nada en su configuración.
+
+Base de datos funcionando. Usaremos MySQL en este tutorial. Necesitarás saber cómo crear una base de datos nueva. CakePHP se encargará del resto.
+
+Nivel básico de PHP. Si estás familiarizado con la programación orientada a objetos, mucho mejor. Aún así puedes seguir desarrollando con tu estilo procedimental si lo prefieres.
+
+Conocimiento sobre patrón MVC. Puedes encontrar una definición rápida aquí: Entendiendo el Modelo - Vista - Controlador. No tengas miedo, sólo es media página.
+(http://book.cakephp.org/2.0/es/cakephp-overview/understanding-model-view-controller.html)
+
--- index.php
+++ index.php
@@ -1,55 +1,43 @@
-<!DOCTYPE html>
-<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="es"> <![endif]-->
-<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="es"> <![endif]-->
-<!--[if IE 8]>    <html class="no-js lt-ie9" lang="es"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="es"> <!--<![endif]-->
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-<title></title>
-<meta name="description" content="">
-<meta name="viewport" content="width=device-width">
-<link rel="stylesheet" href="css/style.css">
-<script src="js/libs/modernizr-2.0.6.min.js"></script>
-</head>
+<?php
+/**
+ * Requests collector.
+ *
+ *  This file collects requests if:
+ *	- no mod_rewrite is available or .htaccess files are not supported
+ *  - requires App.baseUrl to be uncommented in app/Config/core.php
+ *	- app/webroot is not set as a document root.
+ *
+ * PHP 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c), Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         CakePHP(tm) v 0.2.9
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
 
-<body>
-<header>
-    <?php require_once 'includes/header.php'; ?>
-</header>
-<div class="container">
-<div id="content">
-    <div class="left">
-        <nav>
-            <?php require_once 'includes/nav.php'; ?>
-        </nav>
-        <div id="logo-web">
-            <a class="img-container"><img src="" alt=""></a>
-            <p>Construcci&oacute;n libre para la internet</p>
-        </div>
-    </div>
-    <section id="articles">
-	<?php for ($i = 0; $i <= 2; $i++) { ?>
-        <article>
-            <h2><a href="#" title="#">Flisol 2012</a></h2>
-            <time datetime="2001-05-15 19:00">26 Abril, 2012</time>
-            <a href="#" title="#" class="img-container"><img src="img/flisol_600.jpg" alt="Imagen de pruebaa"></a>
-            <p>El Festival Latinoamericano de Instalación de Software Libre es el evento de software libre más importante en América Latina. En esta ocasión, se llevará a cabo el Sábado 28 de Abril del 2012, desde las 8:00 hasta las 3:00pm en la Universidad Centroamericana UCA.</p>
-        </article>
-	<?php } ?>
-    </section>
-</div>
-<footer>
-    <?php require_once 'includes/footer.php'; ?>
-</footer>
-</div>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script>
-$(function() {
-    var width = $(window).width();
-    console.log(width);
-});
-</script>
-<!--<script src="js/loads.js"></script>-->
-</body>
-</html>
+/**
+ *  Get Cake's root directory
+ */
+define('APP_DIR', 'app');
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+define('WEBROOT_DIR', 'webroot');
+define('WWW_ROOT', ROOT . DS . APP_DIR . DS . WEBROOT_DIR . DS);
+
+/**
+ * This only needs to be changed if the "cake" directory is located
+ * outside of the distributed structure.
+ * Full path to the directory containing "cake". Do not add trailing directory separator
+ */
+if (!defined('CAKE_CORE_INCLUDE_PATH')) {
+	define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
+}
+
+require APP_DIR . DS . WEBROOT_DIR . DS . 'index.php';
