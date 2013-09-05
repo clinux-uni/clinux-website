127bed5727ed3dd4f882193778990daddf4d47e9
Samuel J Gutierrez Aviles <search.sama@gmail.com>
2013-09-04 18:50:25 -0600

    Bandera y Barra de Navegacion

--- app/views/layouts/home.blade.php
+++ app/views/layouts/home.blade.php
@@ -9,18 +9,36 @@
 	<head>
 		<meta charset="utf-8" />
 	  	<meta name="viewport" content="width=device-width" />
-	  	<title>Foundation 4</title>
+	  	<title>CLINUX</title>
 
 	  	<!-- CSS -->
 	  	<link rel="stylesheet" href="{{URL::to('/')}}/css/app.css" />
 	</head>
 	<body>
-		<!-- Header and Nav -->
+		<!-- Cabecera and Barra de Navegacion -->
   		<header>
-  			<section class="banner">
-  				<h1>LoGo</h1>
-  			</section>
-  			<div class="menu">Menu goes here - home - links - blah blah</div>
+  			<!-- Bandera del Inicio / Cabecera -->
+  			<section class="bandera"></section>
+  			<!-- Barra de Navegacion -->
+  			<nav class="menu">
+  				<!-- Contenedor del Menu -->
+		        <section class="contenedor">
+		        	<!-- Titlulo de la Barra de Navegacion -->
+		          	<div class="cabecera-menu">
+		          		<a class="titulo-menu" href="{{URL::to('/')}}">CLINUX</a>
+		          	</div>
+
+		          	<!-- Menu -->
+		          	<div class="colapso-menu">
+		          		<!-- Barra de Menu -->
+			            <ul class="barra">
+				            <li class="activo"><a href="#">Home</a></li>
+				            <li><a href="#about">About</a></li>
+				            <li><a href="#contact">Contact</a></li>
+			            </ul>
+		          	</div>
+		        </section>
+		    </nav>
 		</header>
 
 		<div class="content">
